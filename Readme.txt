32feff5d65d01fe1e597b949601bef319a643be4
megapro17 <megapro17@gmail.com>
2023-02-02 21:58:45 +0300

    Initial commit

---
+++ Readme.txt
@@ -0,0 +1,21 @@
+TSC Decrypt
+Расшифровывает файлы из cave story
+Кому это может понадобиться в 2023 неизвестно
+
+Делайте копии, программа перезаписывает все файлы без вопросов
+При неудачной расшифровке файл не изменяется, например если он уже расшифрован
+
+Как использовать:
+Можно ввести целую папку, он найдёт все файлы:
+TSCDecrypt "D:\Games\Cave Story\"
+Или отдельные файлы, сколько угодно:
+TSCDecrypt "Almond.tsc" "Ballo1.tsc" "Ballo2.tsc"
+
+Аналог, но для неё надо ставить Node, а это та ещё хуета https://github.com/jozsefsallai/chie
+
+Эти поебени не работают правильно, из-за ошибки
+https://github.com/slice/tsc_decrypt
+https://gist.github.com/slice/aae504ca921421bf26d077e07136ed22
+
+Скачать собранную программу, .NET не требуется (.NET Native)
+https://github.com/megapro17/TSCDecrypt/releases
---
+++ TSCDecrypt/Program.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.FileSystemGlobbing;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TSCDecrypt
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("usage: tscdecrypt [folder]\n");
+                Console.Error.WriteLine("usage: tscdecrypt [file] [file] [file] ...\n");
+                return -1;
+            }
+            else
+            {
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                IEnumerable<string> matchingFiles;
+                if (args[0].Contains(".tsc"))
+                {
+                    matchingFiles = args;
+                }
+                else
+                {
+                    Matcher matcher = new();
+                    matcher.AddIncludePatterns(new[] { "**/*.tsc" });
+                    var searchDirectory = args[0];
+                    matchingFiles = matcher.GetResultsInFullPath(searchDirectory);
+                    Console.WriteLine($"Найдено tsc файлов: {matchingFiles.Count()}");
+                }
+
+                foreach (string file in matchingFiles)
+                {
+                    string output = Decrypt(file);
+                    if (!Regex.IsMatch(output, @"[\x00\x08\x0B\x0C\x0E-\x1F]"))
+                    {
+                        File.WriteAllText(file, output, new UTF8Encoding(encoderShouldEmitUTF8Identifier: false));
+                        Console.WriteLine($"Обработан: {file}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Ошибка в файле, возможно он уже расшифрован: {file}");
+                    }
+                }
+                return 0;
+            }
+        }
+
+        static string Decrypt(string fileName)
+        {
+            byte[] content = File.ReadAllBytes(fileName);
+            byte[] decrypted = new byte[content.Length];
+            byte key = content[content.Length / 2];
+
+            for (int i = 0; i < decrypted.Length; i++)
+            {
+                if (i == content.Length / 2)
+                {
+                    decrypted[i] = content[i];
+                    continue;
+                }
+                //Console.WriteLine($"{(int)content[i]} - {(int)key} = {(int)(content[i] - key)} ");
+                //Console.Write($"{(sbyte)c} ");
+                decrypted[i] = (byte)(content[i] - key);
+            }
+
+            return Encoding.GetEncoding("windows-1251").GetString(decrypted); ;
+        }
+    }
+}
